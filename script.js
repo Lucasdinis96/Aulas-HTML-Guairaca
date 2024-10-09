719fd67bacf7d09f5c4388e6d2062ca9fe060092
Lucasdinis96 <lucas.dinis1996@gmail.com>
2024-10-08 22:02:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let termo2=2;
 let operacao="multiplicao";
 function calculadora (termo1,termo2,operacao) {
    if (operacao == "soma"){
-    resuktado=termo1+termo2
+    resultado=termo1+termo2
     return console.log(termo1,'+',termo2,"=",resultado);
    }
    if (operacao == "subtracao"){
